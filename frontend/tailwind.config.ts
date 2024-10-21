8ddb839b141b3170428231ebe61805387f24f056
Vaibhav Arora <aroravaibhav817@gmail.com>
2024-10-21 11:08:33 +0530

    added daisy ui

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,11 +1,8 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
@@ -14,6 +11,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["forest"],
+  },
 };
 export default config;
