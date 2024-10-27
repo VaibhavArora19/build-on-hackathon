dc745679747ba87be54118819b7b90dd17ffd10b
Vaibhav Arora <aroravaibhav817@gmail.com>
2024-10-27 16:27:23 +0530

    added positions table

---
+++ frontend/src/app/positions/page.tsx
@@ -0,0 +1,3 @@
+const PositionsPage = () => {};
+
+export default PositionsPage;
---
+++ frontend/src/components/UI/PositonsTable.tsx
@@ -0,0 +1,51 @@
+import { CHAIN_CONFIG } from "@/constants/chainInfo";
+import { protocolNameToImage } from "@/constants/protcolInfo";
+import { TProtocolName } from "@/types/protocol";
+import Image from "next/image";
+
+export type TPositionedAsset = {
+  assetSymbol: string;
+  balance: string;
+  chainId: string;
+  protocolName: string;
+};
+
+const PositionsTable = (props: { assets: TPositionedAsset[] }) => {
+  return (
+    <div className="overflow-x-auto w-[80%] mx-auto">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>Symbol</th>
+            <th>Balance</th>
+            <th>Chain</th>
+            <th>Protocol</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props?.assets?.map((asset, index) => (
+            <tr className="hover" key={index}>
+              <td className="w-[500px]">{asset.assetSymbol}</td>
+              <td className="w-[500px]">{asset.balance}</td>
+              <td className="w-[500px]">
+                <Image src={CHAIN_CONFIG[asset.chainId]?.chainImageUrl} alt="asset image" width={30} height={30} />
+              </td>
+              <td className="w-[300px]">
+                {" "}
+                {<Image src={protocolNameToImage(asset.protocolName as TProtocolName)} alt="asset image" width={30} height={30} />}
+              </td>
+              <td>
+                <button className="btn btn-primary w-full text-md" onClick={() => console.log("hello")}>
+                  Withdraw
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default PositionsTable;
