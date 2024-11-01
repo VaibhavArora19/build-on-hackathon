0a6f4ee29348402ed2d80d121c5cb80b2b7c0896
Vaibhav Arora <aroravaibhav817@gmail.com>
2024-11-01 10:44:05 +0530

    updated basic info comp

--- frontend/src/components/Info/BasicInfo.tsx
+++ frontend/src/components/Info/BasicInfo.tsx
@@ -1,16 +1,22 @@
 const BasicInfo = () => {
   return (
-    <div className="flex ml-[17rem] mb-16 mt-8">
-      <div className="card bg-neutral text-primary-content w-64">
+    <div className="flex ml-[17rem]">
+      <div className="card w-[22rem] bg-base-100 shadow-xl image-full mb-16 mt-8">
+        <figure>
+          <img src="/background.png" alt="Background" />
+        </figure>
         <div className="card-body">
-          <h1 className="font-medium text-[14px] text-[#bebcbc]">Net worth</h1>
-          <p className="font-semibold text-[21px] text-[#bebcbc]">$147.31</p>
+          <h2 className="card-title">Net worth</h2>
+          <p className="text-lg">$147.31</p>
         </div>
       </div>
-      <div className="card bg-neutral text-primary-content w-64 ml-8">
+      <div className="card w-[22rem] bg-base-100 shadow-xl image-full ml-8 mb-16 mt-8">
+        <figure>
+          <img src="/background.png" alt="Background" />
+        </figure>
         <div className="card-body">
-          <h1 className="font-medium text-[14px] text-[#bebcbc]">Net Apy</h1>
-          <p className="font-semibold text-[21px] text-[#bebcbc]">1.5%</p>
+          <h2 className="card-title">Net Apy</h2>
+          <p className="text-lg">1.32%</p>
         </div>
       </div>
     </div>
